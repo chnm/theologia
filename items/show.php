a8f3ce525006ffae6ac8b27df6c0c91b84a210e4
Ken <ken.albers@gmail.com>
2019-06-20 16:26:20 -0400

    Changes for specific metadata

--- items/show.php
+++ items/show.php
@@ -2,8 +2,6 @@
 
 <h1>
     <?php echo metadata($item, array('Dublin Core', 'Title')); ?>
-    <?php if ($englishTitle = metadata($item, array('Item Type Metadata', 'Title (English)'))): ?>
-        <span class="eng"><?php echo $englishTitle; ?></span>
     <?php endif; ?>
 </h1>
 
